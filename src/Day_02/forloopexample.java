d00738d8ca9a46a066e10e019f96042b29b38a95
arunn <arunn@Arun>
2024-02-20 18:42:01 +0530

    forloopexample

---
+++ src/Day_02/forloopexample.java
@@ -0,0 +1,14 @@
+package Day_02;
+
+public class forloopexample {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1000; i>=1;i--)  //
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+}
+
+
